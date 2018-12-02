52ec9bf92a899baffec7d06cefd5500f791a5f3b
Himanshu Nag <himanshu_nag@yahoo.com>
2018-12-02 19:46:57 +0530

    Added Players
    Added 2 Players with movement controls and Enviroment setup.

---
+++ Salad Chef Sim Clone/Assets/Salad Chef Clone/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private string axisH, axisV;
+    public float m_speed;
+
+    private void Start()
+    {
+        axisH = name + "Horizontal";
+        axisV = name + "Vertical";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(m_speed * Input.GetAxis(axisH) * Time.deltaTime, 0f, m_speed * Input.GetAxis(axisV) * Time.deltaTime);
+    }
+}
+
