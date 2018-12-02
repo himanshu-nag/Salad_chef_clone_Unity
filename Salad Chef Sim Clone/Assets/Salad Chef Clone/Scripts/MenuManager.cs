b6a00dddf9e586a534f2bf774c24225bee06d840
Himanshu Nag <himanshu_nag@yahoo.com>
2018-12-02 18:47:18 +0530

    Added Menu Screen
    Added menu screen with play button which loads the game scene with loading progress bar.

---
+++ Salad Chef Sim Clone/Assets/Salad Chef Clone/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject loadingScreen;
+    public Slider loadingBarSlider;
+
+    //TO DO: Passing LevelIndex to load character selection and power upgrade scene in future.
+    public void LoadLevel(int levelIndex)
+    {
+        StartCoroutine(Progress(levelIndex));
+    }
+
+    IEnumerator Progress(int index)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        loadingScreen.SetActive(true);
+        while (!asyncOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            loadingBarSlider.value = progress;
+            yield return null;
+        }
+    }
+}
